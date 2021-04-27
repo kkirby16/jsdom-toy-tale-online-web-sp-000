57097faa04a0b0688d36aabab7f15a5cac32338b
kkirby16 <kkirby16@my.whitworth.edu>
2021-04-26 19:46:27 -0700

    commit tree 2eab0679565f4b5c289996fc9a43674fd0b76c73

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  getToys();
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +14,41 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function getToys() { 
+  let configObject = { 
+    method: "GET",
+    headers: { 
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+};
+
+return fetch("http://localhost:3000/toys", configObject)
+    .then(function(response) { 
+        return response.json(); 
+    })
+    .then(function(object) {
+        object.forEach(element => { 
+          let div = document.createElement("div");
+          div.className = "card";
+          let h2 = document.createElement("h2");
+          h2.innerText = element.name; 
+          let imageElement = document.createElement("img");
+          imageElement.src = element.image; 
+          imageElement.className = "toy-avatar"
+          let p = document.createElement("p");
+          p.innerText = element.likes;
+          let button = document.createElement("button");
+          button.className = "like-btn";
+          button.innerText = "Like <3" 
+          let collectionDiv = document.getElementById("toy-collection");
+          div.appendChild(h2);
+          div.appendChild(imageElement);
+          div.appendChild(p);
+          div.appendChild(button);
+          collectionDiv.appendChild(div);
+        })
+    })
+  
+}
